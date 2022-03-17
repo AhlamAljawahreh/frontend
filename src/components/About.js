8dca56023bae73bf6c883fe3fa65d84b1453bbeb
kholoud ibrahim <ahlam.aljawahreh@gmail.com>
2022-03-17 13:48:02 +0300

    edit

--- src/components/About.js
+++ src/components/About.js
@@ -21,15 +21,15 @@ const About = () => {
       <div className=" mb-5" id="About">
         <div className="card mb-3 bg-light">
           <div className="row " id="About">
-            <div className="col-md-1">{}</div>
-            <div className="col-md-5 ps-5">
+            {/* <div className="col">{}</div> */}
+            <div className="col ps-5">
               <img
                 src="https://res.cloudinary.com/aaaaahlllaaaam/image/upload/v1647432850/Maintenance_2_wxfvm1.gif"
                 className="img-fluid rounded-start"
                 alt="..."
               />
             </div>
-            <div className="col-md-5 bg-light">
+            <div className="col-5 bg-light">
               <div className="card-body ms-0 mt-5">
                 <h1 className="card-title fw-bolder text-center mb-4">About</h1>
                 <p className="card-text fs-5 ">
@@ -42,7 +42,7 @@ const About = () => {
                 </p>
               </div>
             </div>
-            <div className="col-md-1">{}</div>
+            {/* <div className="col-md-1">{}</div> */}
           </div>
         </div>
       </div>
